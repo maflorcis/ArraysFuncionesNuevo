8aa44ad752d3497b514f86083e9c99936c3479ce
floralonso <floralonso@gmail.com>
2022-07-21 16:52:20 -0300

    Ejercicio de los dados explicación profe Emi

--- js/3.ArraysFunciones.js
+++ js/3.ArraysFunciones.js
@@ -44,7 +44,7 @@ let sumas = []; //array vacío
 let pruebaDado1 = [];
 let pruebaDado2 = [];
 
-for( let i=0; i<10; i++) {
+for( let i=0; i<50; i++) {
 
 let dado1 = Math.floor(Math.random()* (6-1+1)+1);
 let dado2 = Math.floor(Math.random()* (6-1+1)+1);
@@ -54,21 +54,24 @@ sumas.push(suma)
 pruebaDado1.push(dado1);
 pruebaDado2.push(dado2);
 }
-document.write('<br>' + 'Resultado prueba del primer dado' + pruebaDado1);
-document.write('<br>' + 'Resultado prueba del segundo dado' + pruebaDado2);
+document.write('<br>' + 'Resultado prueba del primer dado: ' + pruebaDado1);
+document.write('<br>' + 'Resultado prueba del segundo dado: ' + pruebaDado2);
 
-document.write('<br>' + 'Resultados de la suma' + sumas);
+document.write('<br>' + 'Resultados de la suma: ' + sumas);
 
 document.write(`<table>
 <tbody> 
 <tr><td>Resultado </td> <td>Frecuencia</td> <tr>
 `)
+
 for (let resultado =2; resultado <=12; resultado++){
-  document.write(`<tr><td>2 </td> <td>x</td> <tr></tr>`);
+  let contador=0
     for(let posicion = 0; posicion < sumas.length; posicion++){
       if(resultado === sumas [posicion]){
         //sumar las apariciones
+        contador++
       }
     }
+    document.write(`<tr><td>${resultado}</td> <td>${contador}</td> <tr></tr>`)
 }
 document.write(`<tbody></table>`)
